941cc9e8d033584b17edf073efb8f82fb5b6b8df
nvlll <478956990@qq.com>
2017-06-22 10:57:00 +0800

    第五题:star::star:

---
+++ src/_20170621/code/practice6/Happy.java
@@ -0,0 +1,16 @@
+package _20170621.code.practice6;
+
+/**
+ * 第五题
+ *
+ * @author huangxiutao
+ * @create 2017-06-22 10:52
+ **/
+public class Happy {
+    public static void main(String args[]) {
+        float [][] f1 = {{1.2f,2.3f},{4.5f,5.6f}} ;
+        Object oo = f1 ;
+        f1[1] = oo ;
+        System.out.println("Best Wishes "+f1[1]);
+    }
+}
