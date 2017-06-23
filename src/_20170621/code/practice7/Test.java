417e6a386d0630e588aef4e80a14845965ce3031
nvlll <478956990@qq.com>
2017-06-23 14:11:20 +0800

    第七题解析:white_check_mark:

--- src/_20170621/code/practice7/Test.java
+++ src/_20170621/code/practice7/Test.java
@@ -20,5 +20,11 @@ public class Test {
             case 2:
                 System.out.println("two");
         }
+        /**
+         * 解释：
+         * 1.switch后的表达式的类型只能为以下几种：byte、short、char、int、enum、Byte、Short、Integer、Character（在Java1.6中是这样），
+         * 在java1.7后支持了对String的判断
+         * 2.如果switch的case语句后没有break，会继续执行后面的case语句代码和default，直到遇见break或者右花括号。
+         */
     }
 }
