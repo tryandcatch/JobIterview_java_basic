a7bf8fafdfcd5a8b18a8528aa93202b4b5a963c5
nvlll <478956990@qq.com>
2017-06-26 11:14:28 +0800

    第九题:star:

---
+++ src/_20170621/code/practice9/MyClass.java
@@ -0,0 +1,15 @@
+package _20170621.code.practice9;
+
+/**
+ * Created by Huangxiutao on 2017/6/26.
+ */
+public class MyClass {
+    public static void main(String arguments[])     {
+        amethod(arguments);
+    }
+    public void amethod(String[] arguments){
+        System.out.println(arguments);
+        System.out.println(arguments[1]);
+    }
+    
+}
