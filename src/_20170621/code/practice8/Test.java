51b095062aa5be99e08a4555d33ce6dada44b791
nvlll <478956990@qq.com>
2017-06-26 11:04:12 +0800

    第八题:star:

---
+++ src/_20170621/code/practice8/Test.java
@@ -0,0 +1,12 @@
+package _20170621.code.practice8;
+
+/**
+ * Created by Huangxiutao on 2017/6/26.
+ */
+public class Test {
+    static int i;
+    public static void main(String[] args){
+        System.out.println(i);
+    }
+
+}
