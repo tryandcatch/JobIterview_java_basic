62ce6bc563e9053f60dcc732ca7c5e7645dd406e
nvlll <478956990@qq.com>
2017-06-26 11:08:50 +0800

    第八题解释:white_check_mark:

--- src/_20170621/code/practice8/Test.java
+++ src/_20170621/code/practice8/Test.java
@@ -8,5 +8,16 @@ public class Test {
     public static void main(String[] args){
         System.out.println(i);
     }
-
+    /**
+     * 解释:
+     * static修饰的变量（即静态变量）会在编译期间由系统自动初始化并赋予默认值
+     * int>0
+     * byte>0
+     * float>0.0
+     * short>0
+     * double>0.0
+     * char>  (空字符) ASCLL 值为0
+     * long>0
+     * boolean>false
+     */
 }
